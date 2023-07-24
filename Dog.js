55beb78001db689608a80c28138ec5ed895dd6a3
Petr Valenta <petr.valenta.92@gmail.com>
2023-07-24 09:37:26 +0200

    Add getProfileHtml method

--- Dog.js
+++ Dog.js
@@ -1,1 +1,21 @@
 // Create the Dog class here
+
+class Dog {
+    constructor(data) {
+        Object.assign(this, data);
+    }
+
+    getProfileHtml() {
+        const { name, avatar, age, bio } = this;
+
+        return `
+        <div class="profile" style="background-image: url(${avatar})"
+      >
+        <h2 class="profile-name">${name}, ${age}</h2>
+        <p class="profile-bio">${bio}</p>
+      </div>
+        `
+    }
+}
+
+export default Dog;
