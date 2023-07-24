137be9933c16e931a4105c92763eab25a2dd097f
Petr Valenta <petr.valenta.92@gmail.com>
2023-07-24 09:44:34 +0200

    Add render function

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Remember to import the data and Dog class!
+import dogData from "./data.js";
+import Dog from "./Dog.js";
+
+let dogDataIndex = 0;
+
+function getNewProfile() {
+    const nextDogData = dogData[dogDataIndex];
+    dogDataIndex ++;
+    return nextDogData ? new Dog(nextDogData) : {};
+}
+
+function render() {
+    document.getElementById("profile").innerHTML = profile.getProfileHtml();
+}
+
+let profile = getNewProfile();
+
+render();
+
