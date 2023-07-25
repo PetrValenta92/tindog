986f173bf3f5fa6510581af02df0067ee8e965f3
Petr Valenta <petr.valenta.92@gmail.com>
2023-07-25 07:57:12 +0200

    Add lables in profile html

--- Dog.js
+++ Dog.js
@@ -9,11 +9,25 @@ class Dog {
         const { name, avatar, age, bio } = this;
 
         return `
-        <div class="profile" style="background-image: url(${avatar})"
-      >
-        <h2 class="profile-name">${name}, ${age}</h2>
-        <p class="profile-bio">${bio}</p>
-      </div>
+        <div
+            class="profile"
+            style="background-image: url(./images/dog-rex.jpg)"
+          >
+            <img
+              id="like-label"
+              class="label"
+              src="./images/badge-like.png"
+              alt="Like label"
+            />
+            <img
+              id="nope-label"
+              class="label"
+              src="./images/badge-nope.png"
+              alt="Nope label"
+            />
+            <h2 class="profile-name">Rex, 25</h2>
+            <p class="profile-bio">Art. Literature. Natural wine. Yoga.</p>
+          </div>
         `
     }
 }
