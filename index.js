e611adfe6470175b0142aff767d93c130fcf9b77
Petr Valenta <petr.valenta.92@gmail.com>
2023-07-27 09:38:12 +0200

    Render proper label

--- index.js
+++ index.js
@@ -4,34 +4,29 @@ import Dog from "./Dog.js";
 
 let currentProfileIndex = 0;
 
+function likeProfile() {
+    dogProfile.isLiked();
+    dogProfile.isSwiped();
 
-
-document.getElementById("like-btn").addEventListener("click", () => {
-    currentProfileIndex++;
-
-    document.getElementById("like-label").style.display = "block";
-
-    profiles[currentProfileIndex].isSwiped();
-    profiles[currentProfileIndex].isLiked();
-}); 
-
-document.getElementById("reject-btn").addEventListener("click", () => {
-    currentProfileIndex++;
-
-    document.getElementById("nope-label").style.display = "block";
-
-    profiles[currentProfileIndex].isSwiped();
-}); 
-
-function render() {
-    document.getElementById("profile").innerHTML = profiles[currentProfileIndex].getProfileHtml();
+    render();
 }
 
-// document.getElementById("like-btn").addEventListener("click", likeProfile);
-// document.getElementById("reject-btn").addEventListener("click", rejectProfile);
+function rejectProfile() {
+    dogProfile.isSwiped();
+
+    render();
+}
+
+function render() {
+    document.getElementById("profile").innerHTML = dogProfile.getProfileHtml();
+}
+
+document.getElementById("like-btn").addEventListener("click", likeProfile); 
+document.getElementById("reject-btn").addEventListener("click", rejectProfile); 
 
 // Create an array of dog profiles
 const profiles = dogData.map(data => new Dog(data));
+let dogProfile = profiles[currentProfileIndex];
 
 render();
 
