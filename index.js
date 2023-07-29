139768a310a0ec86ed3b6ce0e27daf1330a3fc0f
Petr Valenta <petr.valenta.92@gmail.com>
2023-07-29 22:59:16 +0200

    Add function to restart the app

--- index.js
+++ index.js
@@ -34,23 +34,50 @@ function showNextProfile() {
         }, 1000);
     } else {
         setTimeout(() => {
-            endApp();
+            renderEnd();
+            document.getElementById("reset-btn").addEventListener("click", () => {
+                enableBtns();
+                dogsIndexArray = [ ...Array(dogData.length).keys() ];
+                dogProfile = getNewProfile();
+                render();
+            });
+            
         }, 1000);
     }    
 }
 
 function disableBtns() {
     likeBtn.disabled = true;
+    likeBtn.style.filter = 'grayscale(1)';
+
     rejectBtn.disabled = true;
+    rejectBtn.style.filter = 'grayscale(1)';
+
 }
 
 function enableBtns() {
     likeBtn.disabled = false;
+    likeBtn.style.filter = 'grayscale(0)';
+
     rejectBtn.disabled = false;
+    rejectBtn.style.filter = 'grayscale(0)';
 }
 
-function endApp() {
-    document.getElementById("profile").innerHTML = dogProfile.getEndingHtml();
+function renderEnd() {
+    document.getElementById("profile").innerHTML =  `
+                <div class="end flex">
+                    <img
+                        class="end-gif"
+                        src="https://i.gifer.com/1R8m.gif"
+                        alt="Funny gif from Looney Tones"
+                    />
+                    <p class="end-message">
+                        There's no one around you. Expand your discovery or go Global to
+                        see more.
+                    </p>
+                    <button id="reset-btn" class="reset-btn">Go Global</button>
+                </div>
+            `;
 }
 
 function render() {
